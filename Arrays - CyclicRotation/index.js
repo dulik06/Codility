e62a6df1a0a91e9ce2b21dafcb2cc83ccef43e8f
Alicja <dul.alicja@gmail.com>
2018-09-11 14:59:51 +0200

    Solution improved

--- Arrays - CyclicRotation/index.js
+++ Arrays - CyclicRotation/index.js
@@ -1,17 +1,9 @@
 function solution(A = [], K) {
-    var B = [];
-    var C = [];
-    var D = [];
     var L = A.length;
 
    if(L>K && K>=1) {
-     B = A.slice(0, L-K)
-     C = A.slice(-K)
-     D = C.concat(B)
+     return A.slice(-K).concat(A.slice(0, L-K))
    } else {
-     B = A.slice(0, L - K%L);
-     C = A.slice(L - K%L);
-     D = C.concat(B)
+     return A.slice(L - K%L).concat(A.slice(0, L - K%L))
    }
-    return D;
 }

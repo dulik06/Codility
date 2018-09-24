2919a4caf267e657bb3be0754e31272600986f55
Alicja <dul.alicja@gmail.com>
2018-09-24 11:35:24 +0200

    Final version

--- Arrays - CyclicRotation/index.js
+++ Arrays - CyclicRotation/index.js
@@ -1,9 +1,4 @@
 function solution(A = [], K) {
-    var L = A.length;
-
-   if(L>K && K>=1) {
-     return A.slice(-K).concat(A.slice(0, L-K))
-   } else {
-     return A.slice(L - K%L).concat(A.slice(0, L - K%L))
-   }
+  var L = A.length;
+  return A.slice(L - (K % L)).concat(A.slice(0, L - (K % L)));
 }

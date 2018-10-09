4f8f1093015851432662979f3387d52a9f20170f
Alicja <dul.alicja@gmail.com>
2018-10-09 10:22:36 +0200

    The FrogRiverOne

--- CountingElements-FrogRiverOne/index.js
+++ CountingElements-FrogRiverOne/index.js
@@ -1,13 +1,45 @@
+function solution(X, A) {
+  var obj = new Set();
+  for (var i = 0; i < A.length; i++) {
+    if (A[i] <= X) {
+      obj.add(A[i]);
+      if (obj.size == X) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
 //time runout error
 
 function solution(X, A) {
-	var B = [];
-	var result = 0;
-	for (var i = 1; i <= X; i++) {
-		if(A.indexOf(i) == -1) {
-			return result = -1
-		}
-		B.push(A.indexOf(i))
-	}
- 	return result = Math.max.apply(null, B);
+  var B = [];
+  var result = 0;
+  for (var i = 1; i <= X; i++) {
+    if (A.indexOf(i) == -1) {
+      return (result = -1);
+    }
+    B.push(A.indexOf(i));
+  }
+  return (result = Math.max.apply(null, B));
+}
+
+//time runout error
+
+function solution(X, A) {
+  var number = 1;
+  var index = [];
+  for (var i = 0; i < A.length; i++) {
+    if (A[i] == number && number <= X) {
+      number += 1;
+      index.push(i);
+      i = 0;
+    }
+  }
+  if (index.length == X) {
+    return Math.max.apply(null, index);
+  } else {
+    return -1;
+  }
 }
